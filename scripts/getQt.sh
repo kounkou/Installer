3421215e8f602c9c7e26a9a6fc9cc15e086df1cc
Jacques <gkounkou@gmail.com>
2018-06-04 05:39:48 +0000

    Update getQt.sh

--- scripts/getQt.sh
+++ scripts/getQt.sh
@@ -13,8 +13,10 @@ WEB_SITE="https://download.qt.io/archive/qt"
 
 #######################################
 function checkRemoteFile() {
-   status=$(curl --head --silent "${WEB_SITE}/${QT_MAJ_MIN}/${QT_VERSION}/${INSTALLER}" | head -n 1)
-   if [ echo "$status" | grep -q 404 ]; then
+   local status=$(curl --head --silent "${WEB_SITE}/${QT_MAJ_MIN}/${QT_VERSION}/${INSTALLER}" | head -n 1)
+   local result=`echo "${status}" | grep -q 404`
+   
+   if [ result -ne "" ]; then
       echo "File exists..."
    else
       echo "File ${WEB_SITE}/${QT_MAJ_MIN}/${QT_VERSION}/${INSTALLER} doesn't exist"
